0f263582028b8f84e70944d667152e884515709f
Fa Kmva <fa.kmva@gmail.com>
2023-01-28 09:49:34 +0300

    carsharing module store: mocks and bookCar

--- src/store/modules/carsharing/index.js
+++ src/store/modules/carsharing/index.js
@@ -1,97 +1,39 @@
 import axios from "axios"
+// BackFlag - убрать импорты
+import cars from './mocks'
+import { 
+    brandsDictionary, 
+    modelsDictionary,
+    pricesDictionary,
+    typesDictionary,
+    colorsDictionary
+ } from './mocks'
 
 export default {
     namespaced: true,
     state: {
-        cars: [],
-        brandsDictionary: [
-            {value: 'Audi', label: 'Audi'},
-            {value: 'Bentley', label: 'Bentley'},
-            {value: 'BMW', label: 'BMW'},
-            {value: 'Chevrolet', label: 'Chevrolet'},
-            {value: 'Citroen', label: 'Citroen'},
-            {value: 'Ferrari', label: 'Ferrari'},
-            {value: 'Ford', label: 'Ford'},
-            {value: 'Honda', label: 'Honda'},
-            {value: 'Hyundai', label: 'Hyundai'},
-            {value: 'Kia', label: 'Kia'},
-            {value: 'Lamborghini', label: 'Lamborghini'},
-            {value: 'Lexus', label: 'Lexus'},
-            {value: 'Mazda', label: 'Mazda'},
-            {value: 'Mercedes', label: 'Mercedes'},
-            {value: 'Mitsubishi', label: 'Mitsubishi'},
-            {value: 'Nissan', label: 'Nissan'},
-            {value: 'Opel', label: 'Opel'},
-            {value: 'Peugeot', label: 'Peugeot'},
-            {value: 'Porsche', label: 'Porsche'},
-            {value: 'Renault', label: 'Renault'},
-            {value: 'Skoda', label: 'Skoda'},
-            {value: 'Toyota', label: 'Toyota'},
-            {value: 'Rolls-Royce', label: 'RollsRoyce'},
-            {value: 'Tesla', label: 'Tesla'},
-            {value: 'Volkswagen', label: 'Volkswagen'},
-        ],
-        modelsDictionary: [
-            {
-                title: 'Audi S серия', 
-                items: [
-                    {value: 'S1', label: 'S1',},
-                    {value: 'S2', label: 'S2',},
-                    {value: 'SQ', label: 'SQ',}, 
-                    {value: 'TT', label: 'TT',}, 
-                    {value: 'S3', label: 'S3',},
-                    {value: 'S4', label: 'S4',}
-                ]
-            }
-        ],
-        pricesDictionary: [
-            {value: '1', label: 'до 10 000 ₽'},
-            {value: '2', label: 'от  10 000 ₽ до 20 000 ₽'},
-            {value: '3', label: 'от  20 000 ₽ до 40 000 ₽'},
-            {value: '4', label: 'от  40 000 ₽ до 80 000 ₽'},
-        ],
-        typesDictionary: [
-            {value: '1', label: 'Седан'},
-            {value: '2', label: 'Хэтчбек'},
-            {value: '3', label: 'Универсал'},
-            {value: '4', label: 'Лифтбэк'},
-            {value: '5', label: 'Купе'},
-            {value: '6', label: 'Кабриолет'},
-            {value: '7', label: 'Родстер'},
-            {value: '8', label: 'Тарга'},
-            {value: '9', label: 'Лимузин'},
-            {value: '10', label: 'Стретч'},
-            {value: '11', label: 'Внедорожник'},
-            {value: '12', label: 'Кроссовер'},
-            {value: '13', label: 'Пикап'},
-            {value: '14', label: 'Фургон'},
-            {value: '15', label: 'Минивэн'},
-            {value: '16', label: 'Микроавтобус'},
-            {value: '17', label: 'Автобус'}
-        ],
-        colorsDictionary: [
-            {value: 'white', label: 'Белый'},
-            {value: 'bege', label: 'Бежевый'},
-            {value: 'silver', label: 'Серебристый'},
-            {value: 'grey', label: 'Серый'},
-            {value: 'black', label: 'Черный'},
-            {value: 'yellow', label: 'Желтый'},
-            {value: 'orange', label: 'Оранжевый'},
-            {value: 'red', label: 'Красный'},
-            {value: 'brown', label: 'Коричневый'},
-            {value: 'green', label: 'Зеленый'},
-            {value: 'lightblue', label: 'Голубой'},
-            {value: 'blue', label: 'Синий'},
-            {value: 'purple', label: 'Фиолетовый'},
-            {value: 'pink', label: 'Розовый'},
-        ]
+        // BackFlag - убрать данные из mocks
+        // cars: [],
+        cars,
+        // brandsDictionary: [],
+        brandsDictionary,
+        //modelsDictionary: [],
+        modelsDictionary,
+        //pricesDictionary: [],
+        pricesDictionary,
+        //typesDictionary: [],
+        typesDictionary,
+        //colorsDictionary: [],
+        colorsDictionary
     },
     getters: {
+        getCars: state => state.cars,
+        getCarById: (_, getters) => id => getters.getCars.find(car => car.id == id),
         getBrandsDictionary: state => state.brandsDictionary,
         getModelsDictionary: state => state.modelsDictionary,
         getPricesDictionary: state => state.pricesDictionary,
         getTypesDictionary: state => state.typesDictionary,
-        getColorsDictionary: state => state.colorsDictionary
+        getColorsDictionary: state => state.colorsDictionary,
     },
     mutations: {
         setCars(state, payload) {
@@ -116,7 +58,7 @@ export default {
     actions: {
         async fetchCars({ commit }, filters) {
             try {
-                const response = await axios.post('endpoint', filters ?? null)
+                const response = await axios.get('endpoint', filters ?? null)
                 commit('setCars', response.data);
             } catch (e) {
                 console.log(e)
@@ -124,7 +66,7 @@ export default {
         },
         async fetchFreeCars({ commit }, filters) {
             try {
-                const response = await axios.post('endpoint', filters ?? null)
+                const response = await axios.get('endpoint', filters ?? null)
                 commit('setCars', response.data);
             } catch (e) {
                 console.log(e)
@@ -132,7 +74,7 @@ export default {
         },
         async fetchBrandsDictionary({ commit }) {
             try {
-                const response = await axios.post('endpoint', )
+                const response = await axios.get('endpoint')
                 commit('setBrandsDictionary', response.data);
             } catch (e) {
                 console.log(e)
@@ -140,7 +82,7 @@ export default {
         },
         async fetchModelsDictionary({ commit }) {
             try {
-                const response = await axios.post('endpoint', )
+                const response = await axios.get('endpoint')
                 commit('setModelsDictionary', response.data);
             } catch (e) {
                 console.log(e)
@@ -148,7 +90,7 @@ export default {
         },
         async fetchPricesDictionary({ commit }) {
             try {
-                const response = await axios.post('endpoint', )
+                const response = await axios.get('endpoint')
                 commit('setPricesDictionary', response.data);
             } catch (e) {
                 console.log(e)
@@ -156,7 +98,7 @@ export default {
         },
         async fetchTypesDictionary({ commit }) {
             try {
-                const response = await axios.post('endpoint', )
+                const response = await axios.get('endpoint')
                 commit('setTypesDictionary', response.data);
             } catch (e) {
                 console.log(e)
@@ -164,11 +106,18 @@ export default {
         },
         async fetchColorsDictionary({ commit }) {
             try {
-                const response = await axios.post('endpoint', )
+                const response = await axios.get('endpoint')
                 commit('setColorsDictionary', response.data);
             } catch (e) {
                 console.log(e)
             }
         },
+        async bookCar(_, payload) {
+            try {
+                await axios.post('endpoint', payload)
+            } catch (e) {
+                console.log(e)
+            }
+        }
     },
 }
---
+++ src/store/modules/carsharing/mocks.js
@@ -0,0 +1,185 @@
+export const cars =
+ [
+    { 
+        id: 1, 
+        title: "MERCEDES E220D", 
+        year: 2011, 
+        mileage: 166000, 
+        color: "Коричневый", 
+        price: 16, 
+        brand: 'MercedesBenz', 
+        model: 'w220', 
+        engine: '230ЛC(2л)', 
+        govNumber: 'в006ур777', 
+        address: 'Moscow city', 
+        media: [
+            '@/src/assets/img/single_car.png'
+        ] 
+    },
+    { 
+        id: 2, 
+        title: "MERCEDES E220D", 
+        year: 2011, 
+        mileage: 166000, 
+        color: "Коричневый", 
+        price: 16, 
+        brand: 'MercedesBenz', 
+        model: 'w220', 
+        engine: '230ЛC(2л)', 
+        govNumber: 'в006ур777', 
+        address: 'Moscow city', 
+        media: [
+            '@/src/assets/img/single_car.png'
+        ] 
+    },
+    { 
+        id: 3, 
+        title: "MERCEDES E220D", 
+        year: 2011, 
+        mileage: 166000, 
+        color: "Коричневый", 
+        price: 16, 
+        brand: 'MercedesBenz', 
+        model: 'w220', 
+        engine: '230ЛC(2л)', 
+        govNumber: 'в006ур777', 
+        address: 'Moscow city', 
+        media: [
+            '@/src/assets/img/single_car.png'
+        ] 
+    },
+    { 
+        id: 4, 
+        title: "MERCEDES E220D", 
+        year: 2011, 
+        mileage: 166000, 
+        color: "Коричневый", 
+        price: 16, 
+        brand: 'MercedesBenz', 
+        model: 'w220', 
+        engine: '230ЛC(2л)', 
+        govNumber: 'в006ур777', 
+        address: 'Moscow city', 
+        media: [
+            '@/src/assets/img/single_car.png'
+        ] 
+    },
+    { 
+        id: 5, 
+        title: "MERCEDES E220D", 
+        year: 2011, 
+        mileage: 166000, 
+        color: "Коричневый", 
+        price: 16, 
+        brand: 'MercedesBenz', 
+        model: 'w220', 
+        engine: '230ЛC(2л)', 
+        govNumber: 'в006ур777', 
+        address: 'Moscow city', 
+        media: [
+            '@/src/assets/img/single_car.png'
+        ] 
+    },
+    { 
+        id: 6, 
+        title: "MERCEDES E220D", 
+        year: 2011, 
+        mileage: 166000, 
+        color: "Коричневый", 
+        price: 16, 
+        brand: 'MercedesBenz', 
+        model: 'w220', 
+        engine: '230ЛC(2л)', 
+        govNumber: 'в006ур777', 
+        address: 'Moscow city', 
+        media: [
+            '@/src/assets/img/single_car.png'
+        ] 
+    },
+]
+
+export const brandsDictionary = [
+    {value: 'Audi', label: 'Audi'},
+    {value: 'Bentley', label: 'Bentley'},
+    {value: 'BMW', label: 'BMW'},
+    {value: 'Chevrolet', label: 'Chevrolet'},
+    {value: 'Citroen', label: 'Citroen'},
+    {value: 'Ferrari', label: 'Ferrari'},
+    {value: 'Ford', label: 'Ford'},
+    {value: 'Honda', label: 'Honda'},
+    {value: 'Hyundai', label: 'Hyundai'},
+    {value: 'Kia', label: 'Kia'},
+    {value: 'Lamborghini', label: 'Lamborghini'},
+    {value: 'Lexus', label: 'Lexus'},
+    {value: 'Mazda', label: 'Mazda'},
+    {value: 'Mercedes', label: 'Mercedes'},
+    {value: 'Mitsubishi', label: 'Mitsubishi'},
+    {value: 'Nissan', label: 'Nissan'},
+    {value: 'Opel', label: 'Opel'},
+    {value: 'Peugeot', label: 'Peugeot'},
+    {value: 'Porsche', label: 'Porsche'},
+    {value: 'Renault', label: 'Renault'},
+    {value: 'Skoda', label: 'Skoda'},
+    {value: 'Toyota', label: 'Toyota'},
+    {value: 'Rolls-Royce', label: 'RollsRoyce'},
+    {value: 'Tesla', label: 'Tesla'},
+    {value: 'Volkswagen', label: 'Volkswagen'},
+] 
+
+export const modelsDictionary = [
+    {
+        title: 'Audi S серия', 
+        items: [
+            {value: 'S1', label: 'S1',},
+            {value: 'S2', label: 'S2',},
+            {value: 'SQ', label: 'SQ',}, 
+            {value: 'TT', label: 'TT',}, 
+            {value: 'S3', label: 'S3',},
+            {value: 'S4', label: 'S4',}
+        ]
+    }
+]
+
+export const pricesDictionary = [
+    {value: '1', label: 'до 10 000 ₽'},
+    {value: '2', label: 'от  10 000 ₽ до 20 000 ₽'},
+    {value: '3', label: 'от  20 000 ₽ до 40 000 ₽'},
+    {value: '4', label: 'от  40 000 ₽ до 80 000 ₽'},
+]
+
+export const typesDictionary = [
+    {value: '1', label: 'Седан'},
+    {value: '2', label: 'Хэтчбек'},
+    {value: '3', label: 'Универсал'},
+    {value: '4', label: 'Лифтбэк'},
+    {value: '5', label: 'Купе'},
+    {value: '6', label: 'Кабриолет'},
+    {value: '7', label: 'Родстер'},
+    {value: '8', label: 'Тарга'},
+    {value: '9', label: 'Лимузин'},
+    {value: '10', label: 'Стретч'},
+    {value: '11', label: 'Внедорожник'},
+    {value: '12', label: 'Кроссовер'},
+    {value: '13', label: 'Пикап'},
+    {value: '14', label: 'Фургон'},
+    {value: '15', label: 'Минивэн'},
+    {value: '16', label: 'Микроавтобус'},
+    {value: '17', label: 'Автобус'}
+]
+
+export const colorsDictionary = [
+    {value: 'white', label: 'Белый'},
+    {value: 'bege', label: 'Бежевый'},
+    {value: 'silver', label: 'Серебристый'},
+    {value: 'grey', label: 'Серый'},
+    {value: 'black', label: 'Черный'},
+    {value: 'yellow', label: 'Желтый'},
+    {value: 'orange', label: 'Оранжевый'},
+    {value: 'red', label: 'Красный'},
+    {value: 'brown', label: 'Коричневый'},
+    {value: 'green', label: 'Зеленый'},
+    {value: 'lightblue', label: 'Голубой'},
+    {value: 'blue', label: 'Синий'},
+    {value: 'purple', label: 'Фиолетовый'},
+    {value: 'pink', label: 'Розовый'},
+]
